4048008bd1a323f887437b6f4c2200008c1f475f
Dallas Marlow <dmarlow@twitter.com>
2020-11-05 22:52:58 -0500

    Update backend.tf

--- projects/k8s/backend.tf
+++ projects/k8s/backend.tf
@@ -17,12 +17,12 @@ data "terraform_remote_state" "bastion" {
 	}
 }
 
-data "terraform_remote_state" "eks" {
+data "terraform_remote_state" "eks_compute" {
 	backend = "s3"
 	config = {
 		bucket = "eks-test-tf-backend"
 		region = "us-east-2"
-		key = "eks-cluster/terraform.tfstate"
+		key = "eks-compute/terraform.tfstate"
 		dynamodb_table = "terraform_state_lock"
 	}
 }
