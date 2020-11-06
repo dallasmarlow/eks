09ad74c33339b2f72f8fbb64c79090ad0b9497d4
Dallas Marlow <dmarlow@twitter.com>
2020-11-05 22:35:23 -0500

    Update outputs.tf

--- projects/eks-cluster/outputs.tf
+++ projects/eks-cluster/outputs.tf
@@ -1,5 +1,13 @@
+output "eks_cluster_ca" {
+	value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+}
+
 output "eks_cluster_endpoint" {
 	value = aws_eks_cluster.eks_cluster.endpoint
+}
+
+output "eks_cluster_name" {
+	value = aws_eks_cluster.eks_cluster.name
 }
 
 output "eks_cluster_sg_id" {
