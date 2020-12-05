fc598ac46a1411bb949e08bd7758623be78ec5e6
Dallas Marlow <dmarlow@twitter.com>
2020-12-05 14:52:00 -0500

    update

---
+++ projects/eks-cluster/sync_charts.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -e
+REMOTE_CHARTS=(
+	"https://aws.github.io/eks-charts/|eks|aws-load-balancer-controller|1.1.0"
+	"https://kubernetes.github.io/dashboard/|kubernetes-dashboard|kubernetes-dashboard|3.0.0"
+)
+S3_BUCKET=$(terraform output s3_bucket_helm_repo)
+
+for chart in "${REMOTE_CHARTS[@]}"; do
+	repo_url="$(echo $chart | cut -d '|' -f 1)"
+	repo_name="$(echo $chart | cut -d '|' -f 2)"
+	chart_name="$(echo $chart | cut -d '|' -f 3)"
+	chart_version="$(echo $chart | cut -d '|' -f 4)"
+
+	chart_file="$chart_name-$chart_version.tgz"
+	remote_status="$(aws s3api head-object --bucket $S3_BUCKET --key $chart_file 2>&1)"
+	if [[ "$remote_status" =~ "Not Found" ]]; then
+		# fetch chart
+		if [[ ! -f "$chart_file" ]]; then
+			helm repo add "$repo_name" "$repo_url" --force-update
+			helm pull "$repo_name/$chart_name" --version "$chart_version"
+		fi
+
+		aws s3 cp "$chart_file" "s3://$S3_BUCKET/$chart_file" --acl public-read
+	fi
+done
+
+helm repo index .
+aws s3 cp index.yaml "s3://$S3_BUCKET/index.yaml" --acl public-read
