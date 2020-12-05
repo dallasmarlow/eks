a14cb860b3b7f1586a77cb4d43c9ec5a9d0eff37
Dallas Marlow <dmarlow@twitter.com>
2020-12-05 15:46:45 -0500

    fix

--- projects/route53/backend.tf
+++ projects/route53/backend.tf
@@ -16,3 +16,13 @@ data "terraform_remote_state" "bastion" {
 		dynamodb_table = "terraform_state_lock"
 	}
 }
+
+data "terraform_remote_state" "eks_cluster" {
+	backend = "s3"
+	config = {
+		bucket = "eks-test-tf-backend"
+		region = "us-east-2"
+		key = "eks-cluster/terraform.tfstate"
+		dynamodb_table = "terraform_state_lock"
+	}
+}
