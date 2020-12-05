4d2da03b91ccb230f060669abac1f2c2d33fb186
Dallas Marlow <dmarlow@twitter.com>
2020-12-05 15:41:37 -0500

    update

--- projects/eks-cluster/sync_charts.sh
+++ projects/eks-cluster/sync_charts.sh
@@ -6,6 +6,11 @@ REMOTE_CHARTS=(
 )
 S3_BUCKET=$(terraform output s3_bucket_helm_repo)
 
+if [[ ! -d "./charts" ]]; then
+	mkdir charts
+fi
+cd charts
+
 for chart in "${REMOTE_CHARTS[@]}"; do
 	repo_url="$(echo $chart | cut -d '|' -f 1)"
 	repo_name="$(echo $chart | cut -d '|' -f 2)"
@@ -13,18 +18,18 @@ for chart in "${REMOTE_CHARTS[@]}"; do
 	chart_version="$(echo $chart | cut -d '|' -f 4)"
 	chart_file="$chart_name-$chart_version.tgz"
 
+	# fetch
+	if [[ ! -f "$chart_file" ]]; then
+		helm repo add "$repo_name" "$repo_url" --force-update
+		helm pull "$repo_name/$chart_name" --version "$chart_version"
+	fi
+
 	set +e
 	remote_status="$(aws s3api head-object --bucket $S3_BUCKET --key $chart_file 2>&1)"
 	set -e
 
+	# upload
 	if [[ "$remote_status" =~ "Not Found" ]]; then
-		# fetch chart
-		if [[ ! -f "$chart_file" ]]; then
-			helm repo add "$repo_name" "$repo_url" --force-update
-			helm pull "$repo_name/$chart_name" --version "$chart_version"
-		fi
-
-		# upload
 		aws s3 cp "$chart_file" "s3://$S3_BUCKET/$chart_file" --acl public-read
 	fi
 done
@@ -32,3 +37,5 @@ done
 # index
 helm repo index .
 aws s3 cp index.yaml "s3://$S3_BUCKET/index.yaml" --acl public-read
+
+cd -
