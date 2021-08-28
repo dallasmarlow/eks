2d7b11066828208e0cb3d8a7244fc8bafafd1269
Dallas Marlow <dmarlow@twitter.com>
2021-08-28 17:25:49 -0400

    bastion update

--- projects/bastion/backend.tf
+++ projects/bastion/backend.tf
@@ -1,48 +1,48 @@
 terraform {
-	backend "s3" {
-		bucket = "eks-test-tf-backend"
-		region = "us-east-2"
-		key = "bastion/terraform.tfstate"
-		dynamodb_table = "terraform_state_lock"
-	}
+  backend "s3" {
+    bucket         = "eks-test-tf-backend"
+    region         = "us-east-2"
+    key            = "bastion/terraform.tfstate"
+    dynamodb_table = "terraform_state_lock"
+  }
 }
 
 data "terraform_remote_state" "eks_cluster" {
-	backend = "s3"
-	config = {
-		bucket = "eks-test-tf-backend"
-		region = "us-east-2"
-		key = "eks-cluster/terraform.tfstate"
-		dynamodb_table = "terraform_state_lock"
-	}
+  backend = "s3"
+  config = {
+    bucket         = "eks-test-tf-backend"
+    region         = "us-east-2"
+    key            = "eks-cluster/terraform.tfstate"
+    dynamodb_table = "terraform_state_lock"
+  }
 }
 
 data "terraform_remote_state" "ssh" {
-	backend = "s3"
-	config = {
-		bucket = "eks-test-tf-backend"
-		region = "us-east-2"
-		key = "ssh/terraform.tfstate"
-		dynamodb_table = "terraform_state_lock"
-	}
+  backend = "s3"
+  config = {
+    bucket         = "eks-test-tf-backend"
+    region         = "us-east-2"
+    key            = "ssh/terraform.tfstate"
+    dynamodb_table = "terraform_state_lock"
+  }
 }
 
 data "terraform_remote_state" "terraform_backend" {
-	backend = "s3"
-	config = {
-		bucket = "eks-test-tf-backend"
-		region = "us-east-2"
-		key = "terraform-backend/terraform.tfstate"
-		dynamodb_table = "terraform_state_lock"
-	}
+  backend = "s3"
+  config = {
+    bucket         = "eks-test-tf-backend"
+    region         = "us-east-2"
+    key            = "terraform-backend/terraform.tfstate"
+    dynamodb_table = "terraform_state_lock"
+  }
 }
 
 data "terraform_remote_state" "vpc" {
-	backend = "s3"
-	config = {
-		bucket = "eks-test-tf-backend"
-		region = "us-east-2"
-		key = "vpc/terraform.tfstate"
-		dynamodb_table = "terraform_state_lock"
-	}
+  backend = "s3"
+  config = {
+    bucket         = "eks-test-tf-backend"
+    region         = "us-east-2"
+    key            = "vpc/terraform.tfstate"
+    dynamodb_table = "terraform_state_lock"
+  }
 }
--- projects/bastion/ec2.tf
+++ projects/bastion/ec2.tf
@@ -1,47 +1,51 @@
+locals {
+  name = "${data.terraform_remote_state.eks_cluster.outputs.cluster_name}-bastion"
+}
+
 data "aws_ssm_parameter" "bastion_ami" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-${var.bastion_arch}-gp2"
 }
 
 resource "aws_instance" "bastion" {
-	ami = data.aws_ssm_parameter.bastion_ami.value
-	iam_instance_profile = aws_iam_instance_profile.bastion_instance_profile.name
-	instance_type = var.bastion_instance_type
-	key_name = data.terraform_remote_state.ssh.outputs.eks_test_ec2_keypair_name
-	root_block_device {
-		volume_size = var.bastion_ebs_volume_size
-		encrypted = true
-		kms_key_id = aws_kms_key.bastion_ebs.arn
-	}
-	subnet_id = data.terraform_remote_state.vpc.outputs.eks_test_pub_subnet_b
-	user_data = templatefile(
-		"${path.module}/templates/bootstrap.sh.tpl",
-		{
-			CLUSTER_NAME = var.eks_cluster_name,
-			HELM_URL = var.helm_url,
-			KUBECTL_URL = var.kubectl_url,
-			S3_BUCKET = aws_s3_bucket.bastion_utils.id,
-			TERRAFORM_URL = var.terraform_url})
-	vpc_security_group_ids = [
-		aws_security_group.bastion.id,
-		data.terraform_remote_state.eks_cluster.outputs.eks_cluster_sg_id,
-		data.terraform_remote_state.vpc.outputs.ssh_external_sg_id,
-		data.terraform_remote_state.vpc.outputs.ssh_internal_sg_id,
-	]
-	tags = {
-		Name = "bastion"
-	}
-	depends_on = [
-		aws_s3_bucket_object.ec2_instance_connect_common,
-		aws_s3_bucket_object.ec2_instance_connect_send_key,
-		aws_s3_bucket_object.ec2_instance_connect_ssh,
-		aws_s3_bucket_object.list_eks_admin_token,
-	]
+  ami                  = data.aws_ssm_parameter.bastion_ami.value
+  iam_instance_profile = aws_iam_instance_profile.bastion_instance_profile.name
+  instance_type        = var.bastion_instance_type
+  key_name             = data.terraform_remote_state.ssh.outputs.ec2_keypair_name
+  root_block_device {
+    volume_size = var.bastion_ebs_volume_size
+    encrypted   = true
+    kms_key_id  = aws_kms_key.bastion_ebs.arn
+  }
+  subnet_id = data.terraform_remote_state.vpc.outputs.primary_subnet_ids[0]
+  user_data = templatefile(
+    "${path.module}/templates/bootstrap.sh.tpl",
+    {
+      CLUSTER_NAME = data.terraform_remote_state.eks_cluster.outputs.cluster_name,
+      HELM_URL     = var.helm_url,
+      KUBECTL_URL  = var.kubectl_url,
+      S3_BUCKET    = aws_s3_bucket.bastion_utils.id,
+  TERRAFORM_URL = var.terraform_url })
+  vpc_security_group_ids = [
+    aws_security_group.bastion.id,
+    data.terraform_remote_state.eks_cluster.outputs.cluster_sg_id,
+    data.terraform_remote_state.ssh.outputs.ssh_external_sg_id,
+    data.terraform_remote_state.ssh.outputs.ssh_internal_sg_id,
+  ]
+  tags = {
+    Name = local.name
+  }
+  depends_on = [
+    aws_s3_bucket_object.ec2_instance_connect_common,
+    aws_s3_bucket_object.ec2_instance_connect_send_key,
+    aws_s3_bucket_object.ec2_instance_connect_ssh,
+    aws_s3_bucket_object.list_eks_admin_token,
+  ]
 }
 
 resource "aws_eip" "bastion" {
-	instance = aws_instance.bastion.id
-	vpc = true
-	tags = {
-		Name = "bastion"
-	}
+  instance = aws_instance.bastion.id
+  vpc      = true
+  tags = {
+    Name = local.name
+  }
 }
