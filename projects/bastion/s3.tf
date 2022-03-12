7d4068bd01d8dc9e615f9eef1cc7f2a98ed05a7b
Dallas Marlow <dmarlow@twitter.com>
2022-03-12 08:44:38 -0500

    update S3 object resource types

--- projects/bastion/s3.tf
+++ projects/bastion/s3.tf
@@ -36,25 +36,25 @@ resource "aws_s3_bucket_versioning" "bastion_utils" {
 
 # Objects
 
-resource "aws_s3_bucket_object" "ec2_instance_connect_common" {
+resource "aws_s3_object" "ec2_instance_connect_common" {
   bucket  = aws_s3_bucket.bastion_utils.id
   content = file("${path.module}/scripts/ec2-instance-connect-common.sh")
   key     = "ec2-instance-connect-common.sh"
 }
 
-resource "aws_s3_bucket_object" "ec2_instance_connect_send_key" {
+resource "aws_s3_object" "ec2_instance_connect_send_key" {
   bucket  = aws_s3_bucket.bastion_utils.id
   content = file("${path.module}/scripts/ec2-instance-connect-send-key.sh")
   key     = "ec2-instance-connect-send-key"
 }
 
-resource "aws_s3_bucket_object" "ec2_instance_connect_ssh" {
+resource "aws_s3_object" "ec2_instance_connect_ssh" {
   bucket  = aws_s3_bucket.bastion_utils.id
   content = file("${path.module}/scripts/ec2-instance-connect-ssh.sh")
   key     = "ec2-instance-connect-ssh"
 }
 
-resource "aws_s3_bucket_object" "list_eks_admin_token" {
+resource "aws_s3_object" "list_eks_admin_token" {
   bucket  = aws_s3_bucket.bastion_utils.id
   content = file("${path.module}/scripts/list-eks-admin-token.sh")
   key     = "list-eks-admin-token"
